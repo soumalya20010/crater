234b7d684aba728900c23db43968050df16a214f
GruelingPine185 <adrian.bostic24@gmail.com>
2022-01-12 17:48:20 -0500

    Create version.h

---
+++ inc/shared/version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+
+#define CRATER_VER_MAJOR    0
+#define CRATER_VER_MINOR    0
+#define CRATER_VER_PATCH    1
+
+#define CRATER_VER_STRING   "v0.0.1"
+
+#endif // VERSION_H
